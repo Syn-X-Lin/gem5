95e12fdf6a89796409fe958e81c9aefcfb0e720f
zlfben <zlfben17@gmail.com>
2017-03-16 11:59:36 +0800

    engy: fix bug, use base class for state machine

--- src/engy/EnergyMgmt.py
+++ src/engy/EnergyMgmt.py
@@ -9,4 +9,4 @@ class EnergyMgmt(SimObject):
     path_energy_profile = Param.String(Parent.path_energy_profile, "path to energy profile")
     energy_time_unit = Param.Int(Parent.energy_time_unit, "time unit for energy profile")
     energy_modules = Param.String(Parent.energy_modules, "modules connected to energy port")
-    state_machine = Param.SimpleEnergySM(SimpleEnergySM(), "energy state machine")
+    state_machine = Param.BaseEnergySM(SimpleEnergySM(), "energy state machine")
