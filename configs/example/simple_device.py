9b0c85d64294421a44d90411372966c7246fb12b
zlfben <zlfben17@gmail.com>
2017-05-06 15:54:08 +0800

    vdev: sample script

--- configs/example/simple_device.py
+++ configs/example/simple_device.py
@@ -3,10 +3,13 @@ from m5.objects import *
 
 system = System()
 system.clk_domain = SrcClockDomain()
-system.clk_domain.clock = '1GHz'
+system.clk_domain.clock = '1MHz'
 system.clk_domain.voltage_domain = VoltageDomain()
 system.mem_mode = 'atomic'
 system.mem_ranges = [AddrRange('512MB')]
+system.has_vdev = 1
+system.vdev_ranges = [AddrRange('512MB', '513MB')]
+system.vaddr_vdev_ranges = [AddrRange('1000MB', '1001MB')]
 
 system.cpu = AtomicSimpleCPU()
 
@@ -24,13 +27,16 @@ system.mem_ctrl.port = system.membus.master
 system.system_port = system.membus.slave
 
 system.vdev = VirtualDevice(cpu = system.cpu)
-system.vdev.range = AddrRange('512MB', '1024MB')
+system.vdev.range = system.vdev_ranges[0]
+system.vdev.delay_self = '10ms'
+system.vdev.delay_cpu_interrupt = '1ms'
+system.vdev.delay_set = '100us'
+system.vdev.port = system.membus.master
 
 process = LiveProcess()
-process.cmd = ['tests/test-progs/hello/bin/arm/linux/hello']
+process.cmd = ['tests/test-progs/test_mmap']
 system.cpu.workload = process
 system.cpu.createThreads()
-
 
 
 root = Root(full_system = False, system = system)
